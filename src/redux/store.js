d8ffafb020c0251060f0565f07ba2c671151a90e
Sahil Kondal <sahilkondal128@gmail.com>
2026-01-08 23:37:53 +0530

    makin store.js presist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authslice.js'
+import storage from 'redux-persist/lib/storage' 
+import { persistReducer, persistStore } from 'redux-persist'
+import { combineReducers } from '@reduxjs/toolkit'
+
+const persistConfig = {
+  key: 'root',  
+  storage,      
+}
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer  },
+  reducer: persistedReducer,
 })
+export const persistor = persistStore(store)
